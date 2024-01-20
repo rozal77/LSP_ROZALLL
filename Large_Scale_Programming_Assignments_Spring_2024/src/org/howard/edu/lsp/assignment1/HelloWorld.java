5f9ee99b57c72aef3259c1c2c6733c35d051589b
rozal77 <uniquerojal@gmail.com>
2024-01-19 19:38:29 -0500

    This is Assignment #1.

---
+++ Large_Scale_Programming_Assignments_Spring_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Name : ROJAL SAPKOTA
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		// TODO Auto-generated method stub
+
+	}
+
+}

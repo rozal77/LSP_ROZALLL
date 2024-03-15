32101c29239fd87f5418c53f58bc1be1ceb7cbff
rozal77 <uniquerojal@gmail.com>
2024-03-14 21:08:45 -0400

    Couldn't Complete ALL. Just created an Interface.

---
+++ Large_Scale_Programming_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public interface Range {
+    int lowerBound = 0;
+	int upperBound = 0;
+
+// Returns true if v is ≥ lower bound and ≤ upper bound and false otherwise
+   public boolean contains(int value);
+
+    // Returns true if the receiver contains at least 
+    // one value in common with other, and false otherwise
+   public boolean overlaps(Range other);
+
+    // Returns the number of integers in the range
+    public int size();
+}
